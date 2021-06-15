1cd7c7d2b82d6d8e64d2f039cfadfe2fc231fde7
Falence Lemungoh <falencelemungoh@gmail.com>
2021-06-15 18:10:24 +0100

    Creating our own middleware

--- app.js
+++ app.js
@@ -2,7 +2,21 @@ const express = require('express')
 const fs = require('fs')
 
 const app = express()
+
+
+// Middleware Functions
 app.use(express.json())
+
+app.use((req, res, next) => {
+    console.log('Hello from the middleware 👋')
+    next()
+})
+
+app.use((req, res, next) => {
+    req.requestTime = new Date().toISOString()
+    next()
+})
+
 
 const tours = JSON.parse(
     fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
@@ -11,8 +25,10 @@ const tours = JSON.parse(
 
 // Route Handles
 const getAllTours = (req, res) => {
+    console.log(req.requestTime)
     res.status(200).json({
         status: 'success',
+        requestedAt: req.requestTime,
         results: tours.length,
         data: {
             tours
