f472cf8420c19f96a8f6fa4d9537ef489f1cb6b6
Falence Lemungoh <falencelemungoh@gmail.com>
2021-06-15 20:37:39 +0100

    Chaining multiple middleware functions

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -6,11 +6,21 @@ const tours = JSON.parse(
 
 exports.checkID = (req, res, next, id) => {
     console.log(`Tour id is ${id}`)
-    
+
     if (req.params.id * 1 > tours.length - 1) {
         return res.status(404).json({
             status: 'fail',
             message: 'Invalid ID'
+        })
+    }
+    next()
+}
+
+exports.checkBody = (req, res, next) => {
+    if (!(req.body.name && req.body.price)) {
+        return res.status(400).json({
+            status: 'fail',
+            message: 'Missing name or price'
         })
     }
     next()
