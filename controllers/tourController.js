dff97e0251b4c47da6c1e2f72f8e83e5bf5ee831
Falence Lemungoh <falencelemungoh@gmail.com>
2021-07-02 19:23:46 +0100

    Implement filtering

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,7 +2,24 @@ const Tour = require('./../models/tourModel')
 
 exports.getAllTours = async (req, res) => {
     try {
-        const tours = await Tour.find()
+        // BUILD THE QUERY
+        // Exclude some query parameters
+        const queryObj = { ...req.query }
+        const excludedFields = ['page', 'sort', 'limit', 'fields']
+        excludedFields.forEach( el => delete queryObj[el])
+
+        const query = Tour.find(queryObj) // without an await, it returns a QUERY
+
+        // const query = await Tour.find()
+        //     .where('duration')
+        //     .equals(5)
+        //     .where('difficulty')
+        //     .equals('easy')
+
+        // EXECUTE THE QUERY
+        const tours = await query
+
+        // SEND RESPONSE
         res.status(200).json({
             status: 'success',
             results: tours.length,
