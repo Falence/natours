6813f6feb33ce3973e66629e225eba46e173f054
Falence Lemungoh <falencelemungoh@gmail.com>
2021-07-02 18:00:58 +0100

    Implement basic tour CRUD operations

--- server.js
+++ server.js
@@ -8,42 +8,10 @@ mongoose
     .connect(process.env.DATABASE_LOCAL, {
         useNewUrlParser: true,
         useCreateIndex: true,
-        useFindAndModify: true,
+        useFindAndModify: false,
         useUnifiedTopology: true
     })
     .then(() => console.log('DB connection successful!!'))
-
-
-const tourSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, 'A tour must have a name'],
-        unique: true
-    },
-    rating: {
-        type: Number,
-        default: 4.5
-    },
-    price: {
-        type: Number,
-        required: [true, 'A tour must have a price']
-    }
-})
-
-const Tour = mongoose.model('Tour', tourSchema)
-
-const testTour = new Tour({
-    name: 'The Park Camper',
-    price: 999
-})
-testTour
-    .save()
-    .then(doc => {
-        console.log(doc)
-    })
-    .catch(err => {
-        console.log('ERROR 🔥: ', err)
-    })
 
 
 const port = process.env.PORT || 3000
