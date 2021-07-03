cdbf9a53ca0c4519d816617e30eab38c743715e8
Falence Lemungoh <falencelemungoh@gmail.com>
2021-07-03 11:24:26 +0100

    Limiting fields

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -3,17 +3,33 @@ const Tour = require('./../models/tourModel')
 exports.getAllTours = async (req, res) => {
     try {
         // BUILD THE QUERY
-        // 1) Filtering
+        // 1A) Filtering
         const queryObj = { ...req.query }
         const excludedFields = ['page', 'sort', 'limit', 'fields']
         excludedFields.forEach( el => delete queryObj[el])
 
-        // 2) Advanced filtering
+        // 1B) Advanced filtering
         let queryStr = JSON.stringify(queryObj)
         queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`)
-
-        const query = Tour.find(JSON.parse(queryStr)) // without an await, it returns a QUERY
         
+        let query = Tour.find(JSON.parse(queryStr)) // without an await, it returns a QUERY
+        
+        // 2) Sorting
+        if (req.query.sort) {
+            const sortBy = req.query.sort.split(',').join(' ')
+            query = query.sort(sortBy)
+        } else {
+            query = query.sort('-createdAt') // newest first
+        }
+
+        // 3) Field limiting
+        if (req.query.fields) {
+            const fields = req.query.fields.split(',').join(' ')
+            query = query.select(fields)
+        } else {
+            query = query.select('-__v') 
+        }
+
         // EXECUTE THE QUERY
         const tours = await query
 
