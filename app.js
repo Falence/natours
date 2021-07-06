ba2f68ebbba70a5e866df6e46ff2c574d048c2b2
Falence Lemungoh <falencelemungoh@gmail.com>
2021-07-06 15:28:43 +0100

    Handling errors for undefined routes

--- app.js
+++ app.js
@@ -12,15 +12,8 @@ const app = express()
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
 }
-
 app.use(express.json())
 app.use(express.static(`${__dirname}/public`))
-
-app.use((req, res, next) => {
-    console.log('Hello from the middleware 👋')
-    next()
-})
-
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString()
     next()
@@ -30,6 +23,31 @@ app.use((req, res, next) => {
 // Routes
 app.use('/api/v1/tours', tourRouter)
 app.use('/api/v1/users', userRouter)
+
+// For undefined routes
+app.all('*', (req, res, next) => {
+    // res.status(404).json({
+    //     status: 'fail',
+    //     message: `Can't find ${req.originalUrl} on this server!`
+    // })
+
+    const err = new Error(`Can't find ${req.originalUrl} on this server!`)
+    err.status = 'fail'
+    err.statusCode = 404
+
+    next(err)
+}) 
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+    err.statusCode = err.statusCode || 500
+    err.status = err.status || 'error'
+
+    res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message
+    })
+})
 
 
 module.exports = app
