52e7f7a66e028a5a8446223eb3baddee6a0c7d5c
Falence Lemungoh <falencelemungoh@gmail.com>
2021-07-12 19:03:04 +0100

    Handling unhandled rejections

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,11 +1,68 @@
+const AppError = require('./../utils/appError')
+
+
+const handleCastErrorDB = err => {
+    const message = `Invalid ${err.path}: ${err.value}`
+    return new AppError(message, 400)
+}
+
+const handleDuplicateFieldsDB = err => {
+    const message = `Duplicate field value: '${err.keyValue.name}'. Please use another value`
+    return new AppError(message, 400)
+}
+
+const handleValidationErrorDB = err => {
+    const errors = Object.values(err.errors).map(el => el.message)
+    const message = `Invalid input data. ${errors.join('. ')}`
+    return new AppError(message, 400)
+}
+
+
+const sendErrorDev = (err, res) => {
+    res.status(err.statusCode).json({
+        status: err.status,
+        error: err,
+        message: err.message,
+        stack: err.stack
+    })
+}
+
+const sendErrorProd = (err, res) => {
+    // Send only operational errors to client on production
+    if (err.isOperational) {
+        res.status(err.statusCode).json({
+            status: err.status,
+            message: err.message
+        })
+
+    // programming errors or other unknown errors: don't leak error details on prod
+    } else {
+        // Log the error
+        console.error('ERROR: ', err)
+
+        // Send generated message
+        res.status(500).json({
+            status: 'error',
+            message: 'Something went very wrong!'
+        })
+    }
+}
+
+
 module.exports = (err, req, res, next) => {
     // console.log(err.stack)
 
     err.statusCode = err.statusCode || 500
     err.status = err.status || 'error'
 
-    res.status(err.statusCode).json({
-        status: err.status,
-        message: err.message
-    })
+    if (process.env.NODE_ENV === 'development') {
+        sendErrorDev(err, res)
+    } else if (process.env.NODE_ENV === 'production') {
+        // let error = { ...err }
+        if (err.name === 'CastError') err = handleCastErrorDB(err)
+        if (err.code === 11000) err = handleDuplicateFieldsDB(err)
+        if (err.name === 'ValidationError') err = handleValidationErrorDB(err)
+
+        sendErrorProd(err, res)
+    }
 }
--- server.js
+++ server.js
@@ -15,8 +15,15 @@ mongoose
 
 
 const port = process.env.PORT || 3000
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(`App running on port: ${port}...`)
 })
 
-// From Chrome Dev tools
+// handle unhandled promise rejection error
+process.on('unhandledRejection', err => {
+    console.log(err.name, err.message)
+    console.log('UNHANDLED REJECTION! 🔥 Shutting down...')
+    server.close(() => {
+        process.exit(1)
+    })
+})
