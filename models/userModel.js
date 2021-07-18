0322334d9dee42cd3a8b3b5cbeb3b79cb5d7c54b
Falence Lemungoh <falencelemungoh@gmail.com>
2021-07-18 20:06:48 +0100

    password management

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
+const bcrypt = require('bcryptjs')
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -21,8 +22,23 @@ const userSchema = new mongoose.Schema({
     },
     passwordConfirm: {
         type: String,
-        required: [true, 'Please confirm your password!']
+        required: [true, 'Please confirm your password!'],
+        validate: {
+            // this works only on CREATE and SAVE !!!
+            validator: function(el) {
+                return el === this.password
+            },
+            message: 'Passwords are not the same!'
+        }
     }
+})
+
+userSchema.pre('save', async function(next) {
+    if (!this.isModified('password')) return next() // hash only when the password field is modified
+
+    this.password = await bcrypt.hash(this.password, 12)
+    this.passwordConfirm = undefined    // delete the 'passwordConfirm' field. we needed the field initially just for verification
+    next()
 })
 
 const User = mongoose.model('User', userSchema)
