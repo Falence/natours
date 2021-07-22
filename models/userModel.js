c1a10464daddd3b29e4b6998631ec385fa454031
Falence Lemungoh <falencelemungoh@gmail.com>
2021-07-23 00:15:59 +0100

    Sending emails with nodemailer

--- models/userModel.js
+++ models/userModel.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto')
 const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcryptjs')
@@ -37,7 +38,9 @@ const userSchema = new mongoose.Schema({
             message: 'Passwords are not the same!'
         }
     },
-    passwordChangedAt: Date
+    passwordChangedAt: Date,
+    passwordResetToken: String,
+    passwordResetExpires: Date
 })
 
 // hash password
@@ -64,6 +67,22 @@ userSchema.methods.changedPasswordAfter = function(JWTTimestamp) {
     return false
 }
 
+userSchema.methods.createPasswordResetToken = function() {
+    // create random string
+    const resetToken = crypto.randomBytes(32).toString('hex')
+
+    // encrypt for storage in DB
+    this.passwordResetToken = crypto
+        .createHash('sha256')
+        .update(resetToken)
+        .digest('hex')
+
+    console.log({resetToken}, this.passwordResetToken)
+
+    this.passwordResetExpires = Date.now() + (10 * 60 * 1000)
+
+    return resetToken
+}
 
 const User = mongoose.model('User', userSchema)
 
