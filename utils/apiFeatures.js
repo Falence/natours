25fd0e276abe6f195abefaf023b9b9dbb540d890
Falence Lemungoh <falencelemungoh@gmail.com>
2021-07-24 21:34:18 +0100

    prevent parameter pollution

--- utils/apiFeatures.js
+++ utils/apiFeatures.js
@@ -6,6 +6,7 @@ class APIFeatures {
 
     filter() {
         // Filtering
+        console.log(this.queryString)
         const queryObj = { ...this.queryString }
         const excludedFields = ['page', 'sort', 'limit', 'fields']
         excludedFields.forEach( el => delete queryObj[el])
