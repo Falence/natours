7f65036ce4c96b22122324a1d0ee26c7d7e35b08
Falence Lemungoh <falencelemungoh@gmail.com>
2021-07-24 20:41:49 +0100

    implements rate limiting

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const rateLimit = require('express-rate-limit')
 
 const AppError = require('./utils/appError')
 const globalErrorHandler = require('./controllers/errorController')
@@ -10,10 +11,18 @@ const userRouter = require('./routes/userRoutes')
 const app = express()
 
 
-// Middleware Functions
+// Globasl Middleware Functions
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
 }
+
+const limiter = rateLimit({
+    max: 100,
+    windowMs: 60 * 60 * 1000,
+    message: 'Too many requests from this IP! Please try again in an hour!'
+})
+app.use('/api', limiter) // limit the number of requests to the api routes to 100
+
 app.use(express.json())
 app.use(express.static(`${__dirname}/public`))
 app.use((req, res, next) => {
