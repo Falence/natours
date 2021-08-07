06da02a9acfd213244db58f3e6cc3324d5e239a1
Falence Lemungoh <falencelemungoh@gmail.com>
2021-08-07 03:35:29 +0100

    Nested routes with express

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const tourController = require('./../controllers/tourController')
 const authController = require('./../controllers/authController')
-const reviewController = require('./../controllers/reviewController')
+const reviewRouter = require('./reviewRoutes')
 
 const router = express.Router()
+
+
+router.use('/:tourId/reviews', reviewRouter) // redirect to reviewRouter
 
 router
     .route('/top-5-cheap')
@@ -29,13 +32,6 @@ router
     .delete(authController.protect, authController.restrictTo('admin', 'lead-guide'), tourController.deleteTour)
 
 
-router
-    .route('/:tourId/reviews')
-    .post(
-        authController.protect,
-        authController.restrictTo('user'),
-        reviewController.createReview
-    )
 
 module.exports = router
 
