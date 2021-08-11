6e66c213245cc3b8d1ea462314538651d1ea014a
Falence Lemungoh <falencelemungoh@gmail.com>
2021-08-11 19:57:49 +0100

    preventing duplicate reviews

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -30,6 +30,9 @@ const reviewSchema = new mongoose.Schema({
     toObject: { virtuals: true }
 })
 
+// user can create only one review
+reviewSchema.index({ tour: 1, user: 1 }, { unique: true })
+
 reviewSchema.pre(/^find/, function(next) {
     // this.populate({
     //     path: 'tour',
