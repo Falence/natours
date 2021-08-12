41509ed802b9577a10dc6aad3f8ed9cefca182a2
Falence Lemungoh <falencelemungoh@gmail.com>
2021-08-12 21:56:54 +0100

    extending our base template with blocks

--- app.js
+++ app.js
@@ -70,10 +70,22 @@ app.use((req, res, next) => {
 
 
 // Routes
-app.get('/', (req, res, next) => {
+app.get('/', (req, res) => {
     res.status(200).render('base', {
-        tour: "The Forest Hiker",
-        user: "Falence"
+        tour: 'The Forest Hiker',
+        user: 'Falence'
+    })
+})
+
+app.get('/overview', (req, res) => {
+    res.status(200).render('overview', {
+        title: 'All Tours'
+    })
+})
+
+app.get('/tour', (req, res) => {
+    res.status(200).render('tour', {
+        title: 'The Forest Hiker'
     })
 })
 
