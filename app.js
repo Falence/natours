879571432d685e1f31d39fd6ba5013f44be90ab8
Falence Lemungoh <falencelemungoh@gmail.com>
2021-08-12 20:43:59 +0100

    First steps with pug

--- app.js
+++ app.js
@@ -71,7 +71,10 @@ app.use((req, res, next) => {
 
 // Routes
 app.get('/', (req, res, next) => {
-    res.status(200).render('base')
+    res.status(200).render('base', {
+        tour: "The Forest Hiker",
+        user: "Falence"
+    })
 })
 
 app.use('/api/v1/tours', tourRouter)
