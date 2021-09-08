27c0d371beeddd261f354f25921921a50bd519bb
Falence Lemungoh <falencelemungoh@gmail.com>
2021-09-08 18:16:29 +0100

    Configuring multer

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,6 @@
-const multer = require('multer')
 const express = require('express')
 const userController = require('./../controllers/userController')
 const authController = require('./../controllers/authController')
-
-const upload = multer({ dest: 'public/img/users' })
 
 const router = express.Router()
 
@@ -23,7 +20,7 @@ router.patch(
 )
 
 router.get('/me', userController.getMe, userController.getUser)
-router.patch('/updateMe', upload.single('photo'), userController.updateMe)
+router.patch('/updateMe', userController.uploadUserPhoto, userController.updateMe)
 router.delete('/deleteMe', userController.deleteMe)
 
 
