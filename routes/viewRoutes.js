44a62f11ef39a54fef2265c643ecbba45d79d25d
Falence Lemungoh <falencelemungoh@gmail.com>
2021-11-12 21:28:42 +0100

    creating new bookings on checkout success

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const viewController = require('./../controllers/viewController')
 const authController = require('./../controllers/authController')
+const bookingController = require('./../controllers/bookingController')
 
 const router = express.Router()
 
-router.get('/', authController.isLoggedIn, viewController.getOverview)
+router.get('/',
+  bookingController.createBookingCheckout,
+  authController.isLoggedIn,
+  viewController.getOverview
+)
 router.get('/tours/:slug', authController.isLoggedIn, viewController.getTour)
 router.get('/signup', authController.isLoggedIn, viewController.getSingupForm)
 router.get('/login', authController.isLoggedIn, viewController.getloginForm)
