49438b9870e636e7a4433a7164f32ff6f70ffda5
Falence Lemungoh <falencelemungoh@gmail.com>
2021-11-13 12:51:56 +0100

    fix logout redirect

--- public/js/login.js
+++ public/js/login.js
@@ -31,7 +31,7 @@ export const logout = async () => {
             url: '/api/v1/users/logout'
         })
         showAlert('success', 'Logged out successfully')
-        if (res.data.status === 'success') location.reload(true) // reloads from the server and not from the browser's cache
+        if (res.data.status === 'success') location.assign('/') // reloads from the server and not from the browser's cache
     } catch(err) {
         showAlert('error', 'Error logging out! Try again!')
     }
